34e8f5f1adbf459462430f9f240db925f0e136d1
Denis Costello <G00396309@atu.ie>
2024-03-26 14:54:24 +0000

    First commit

---
+++ src/main/java/ie/atu/scrapedfood/ScrapedFoodApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.scrapedfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapedFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapedFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/scrapedfood/ScrapedFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.scrapedfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapedFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
